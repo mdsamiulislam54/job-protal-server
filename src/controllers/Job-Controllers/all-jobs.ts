8f194e60ce46bd84502667551937de2f8f615a16
mdsamiulislam54 <samiulm5332@gmail.com>
2025-11-09 18:35:16 +0600

    update all-jobs api endpoint

--- src/controllers/Job-Controllers/all-jobs.ts
+++ src/controllers/Job-Controllers/all-jobs.ts
@@ -1,19 +1,59 @@
 import { Request, Response, NextFunction } from "express"
 import JobsModel from "../../models/JobModel/jobModel"
+
 export const AllJobs = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const page = parseInt(req.query.page as string) || 1
         const limit = parseInt(req.query.limit as string) || 10
         const category = (req.query.category as string) || "All"
+        const filterData = JSON.parse(req.query.filter as string) || {}
+        const { search, location, min, max } = filterData
 
-        const filter: any = {}
+
+        const query: any = {}
         if (category !== "All") {
-            filter.category = category
+            query.category = category
         }
 
-        const total = await JobsModel.countDocuments(filter);
-        const jobs = await JobsModel.find(filter).sort({ createdAt: -1 }).limit(limit).skip((page - 1) * limit);
+        if (search) {
+            query.$or = [
+                { title: { $regex: search, $options: "i" } },
+                { jobDescription: { $regex: search, $options: "i" } }
+            ]
+        }
 
+        if (location) {
+            query.location = { $regex: location, $options: "i" };
+        }
+
+
+        if (min && max) {
+            if (min && max) {
+                const minSalary = Number(min);
+                const maxSalary = Number(max);
+                query['salaryRange.min'] = { $gte: minSalary };
+                query['salaryRange.max'] = { $lte: maxSalary };
+            }
+
+        }
+
+        // const aggregationPipeline = [
+        //     { $query: query },
+        //     { $sort: { createdAt: -1 as -1 } },
+        //     { $skip: (page - 1) * limit },
+        //     { $limit: limit }
+        // ]
+
+
+
+        const [jobs, total, uniqueCategory, uniqueLocation] = await Promise.all([
+            JobsModel.find(query).sort({createdAt: -1 }).limit((page -1)* limit).limit(limit),
+            JobsModel.countDocuments(query),
+        
+            JobsModel.distinct("category"),
+            JobsModel.distinct("location"),
+        ])
+     
         const jobsData = jobs.map((item) => ({
             _id: item._id,
             salaryRange: `৳ ${item.salaryRange.min} — ${item.salaryRange.max}`,
@@ -29,10 +69,12 @@ export const AllJobs = async (req: Request, res: Response, next: NextFunction) =
             success: true,
             total: Math.floor(total / limit),
             jobsData,
+            uniqueCategory,
+            uniqueLocation
         })
 
 
     } catch (error) {
-
+        next(error)
     }
 }
